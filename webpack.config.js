5f86acf45a6eb405adac949259f86b530b9dac19
lwg1103 <P8zQ7fZDOyExx2J>
2020-06-19 23:39:50 +0200

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,3 @@
-var WebpackPwaManifest = require('webpack-pwa-manifest');
-
-plugins: [
-  new WebpackPwaManifest({
-    name: 'My Progressive Web App',
-    short_name: 'MyPWA',
-    description: 'My awesome Progressive Web App!',
-    background_color: '#ffffff',
-    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-    icons: [
-      {
-        src: path.resolve('src/assets/icon.png'),
-        sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-      },
-      {
-        src: path.resolve('src/assets/large-icon.png'),
-        size: '1024x1024' // you can also use the specifications pattern
-      },
-      {
-        src: path.resolve('src/assets/maskable-icon.png'),
-        size: '1024x1024',
-        purpose: 'maskable'
-      }
-    ]
-  })
-];
-
 var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
