a2b9647e0432a815b10bf0fa7475f9789b3792f1
lwg1103 <P8zQ7fZDOyExx2J>
2020-07-08 22:25:22 +0200

    location provider

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
+import {Route, Switch, Redirect, Link, withRouter} from 'react-router-dom';
 import Map from './Map';
 import Position from './Position';
 import CheckButton from './CheckButton';
@@ -7,9 +7,10 @@ import BackButton from './BackButton';
 import Screen from '../dict/Screen';
 
 class Home extends Component {
-    
+
     constructor(props) {
         super(props);
+
         this.state = {
             long: 17.05629,
             lat: 51.08613,
@@ -20,74 +21,91 @@ class Home extends Component {
         this.handleLongChange = this.handleLongChange.bind(this);
         this.handleWeatherConditionsChange = this.handleWeatherConditionsChange.bind(this);
         this.navigateTo = this.navigateTo.bind(this);
+        this.updatePosition = this.updatePosition.bind(this);
+        this.savePosition = this.savePosition.bind(this);
+
+        this.updatePosition();
     }
-      
+
+    updatePosition()
+    {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.savePosition);
+        }
+    }
+
+    savePosition(position) {
+        this.setState({
+            lat: (position.coords.latitude).toFixed(5),
+            long: (position.coords.longitude).toFixed(5),
+        })
+    }
+
     handleLatChange(event) {
         this.setState({
             lat: event.target.value,
         });
     }
-    
+
     handleLongChange(event) {
         this.setState({
             long: event.target.value
         });
     }
-    
+
     handleWeatherConditionsChange(result) {
-        console.log('cond change', result);
         this.setState({
             conditions: result,
             screen: Screen.RecommendationDetails
         });
     }
-    
+
     navigateTo(screen) {
         this.setState({
             screen: screen
         })
     }
-    
+
     render() {
         switch (this.state.screen) {
             case Screen.Home:
                 return this.renderHome();
             case Screen.RecommendationDetails:
                 return this.renderRecommendationDetails();
-            
+
         }
     }
-    
+
     renderHome() {
         return (
-           <div>
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                   <Link className={"navbar-brand"} to={"/"}> Runners Weather 2.0 </Link>
-               </nav>
-                <Map /> 
-                <Position lat={this.state.lat} long={this.state.long} onLatChange={this.handleLatChange} onLongChange={this.handleLongChange}/>
-                <CheckButton lat={this.state.lat} long={this.state.long} onSuccess={this.handleWeatherConditionsChange}/>
-           </div>
-        )
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <Link className={"navbar-brand"} to={"/"}> Runners Weather 2.0 </Link>
+                    </nav>
+                    <Map lat={this.state.lat} long={this.state.long} /> 
+                    <Position lat={this.state.lat} long={this.state.long} onLatChange={this.handleLatChange} onLongChange={this.handleLongChange}/>
+                    <CheckButton lat={this.state.lat} long={this.state.long} onSuccess={this.handleWeatherConditionsChange}/>
+                </div>
+                )
     }
-    
+
     renderRecommendationDetails() {
         return (
-           <div>
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                   <Link className={"navbar-brand"} to={"/"}> Runners Weather 2.0 </Link>
-               </nav>
-               <BackButton callback={this.navigateTo} target={Screen.Home}/>
-               <p>PM 2.5: {this.state.conditions.pm25}</p>
-               <p>PM 10: {this.state.conditions.pm10}</p>
-               <p>Temp: {this.state.conditions.temperature} 'C</p>
-               <p>Weather: {this.state.conditions.type.name}</p>
-               <p>Wind: {this.state.conditions.wind} m/s</p>
-               <p>Humidity: {this.state.conditions.humidity} %</p>
-               <p>Decision: {this.state.conditions.decision.name}</p>
-           </div>
-        )
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <Link className={"navbar-brand"} to={"/"}> Runners Weather 2.0 </Link>
+                    </nav>
+                    <BackButton callback={this.navigateTo} target={Screen.Home}/>
+                    <p>PM 2.5: {this.state.conditions.pm25}</p>
+                    <p>PM 10: {this.state.conditions.pm10}</p>
+                    <p>Temp: {this.state.conditions.temperature} 'C</p>
+                    <p>Weather: {this.state.conditions.type.name}</p>
+                    <p>Wind: {this.state.conditions.wind} m/s</p>
+                    <p>Humidity: {this.state.conditions.humidity} %</p>
+                    <p>Decision: {this.state.conditions.decision.name}</p>
+                </div>
+                )
     }
 }
-    
+
 export default Home;
--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-    
+import GoogleMapReact from 'google-map-react';
+
 class Map extends Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {props};
+    }
+
     render() {
         return(
-            <div className="map">
-            Here will be the map
-            </div>
-        )
+                <div className="map" style={{height: '50vh', width: '100%'}}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{key: "AIzaSyAJWgwdS-Luy3s1l40AvBPlHfZGdsV0rk4"}}
+                        defaultCenter={{lat: 1, long: 1}}
+                        center={{lat: this.props.lat, long: this.props.long}}
+                        defaultZoom={11}
+                        onGoogleApiLoaded={({map, maps}) => console.log(map, maps)}
+                        yesIWantToUseGoogleMapApiInternals
+                        >
+                    </GoogleMapReact>
+                </div>
+                )
     }
 }
 export default Map;
