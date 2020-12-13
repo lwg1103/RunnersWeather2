99dbffeb242705454cd598c893f4c2bb21717dc8
lwg1103 <lwg1103@wp.pl>
2020-12-13 17:40:35 +0100

    migration fix

--- migrations/Version20201213161734.php
+++ migrations/Version20201213161734.php
@@ -21,7 +21,7 @@ final class Version20201213161734 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user_api_access (id INT AUTO_INCREMENT NOT NULL, api_key VARCHAR(32) NOT NULL, active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_27F9AA38C912ED9D (api_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD api_access_id INT DEFAULT NULL, CHANGE roles roles JSON NOT NULL, CHANGE password password VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD api_access_id INT DEFAULT NULL, CHANGE password password VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649323EC2B4 FOREIGN KEY (api_access_id) REFERENCES user_api_access (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649323EC2B4 ON user (api_access_id)');
     }
